18395ce55b50c1600d66c086f6a2269579ab7943
Yaroslav <dzisyuk.yari@gmail.com>
2022-01-06 00:14:16 +0200

    Initial commit

---
+++ src/js/modules/modals.js
@@ -0,0 +1,28 @@
+const modals = () => {
+    function bindModal(trigger, modal, close) {
+        trigger.addEventListener('click', (e) => {
+            if (e.target) {
+                e.preventDefault();
+            }
+
+            modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+
+        close.addEventListener('click', (e) => {
+            if (e.target) {
+                e.preventDefault();
+            }
+
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        });
+    }
+    const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
+              modalEngineer = document.querySelector('.popup_engineer'),
+              modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+
+    bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+};
+
+export default modals;
