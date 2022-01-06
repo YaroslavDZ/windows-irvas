21e8e3087e5aa630b151d13035183e38dcb1ca8d
Yaroslav <dzisyuk.yari@gmail.com>
2022-01-07 00:32:42 +0200

    Implemented a part of tabs on the website

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -12,7 +12,6 @@ const modals = () => {
     
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
-                // document.body.classList.add('modal-open');
             });
         });
 
@@ -23,14 +22,12 @@ const modals = () => {
 
             modal.style.display = 'none';
             document.body.style.overflow = '';
-            // document.body.classList.remove('modal-open');
         });
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
-                // document.body.classList.remove('modal-open');
             }
         });
     }
