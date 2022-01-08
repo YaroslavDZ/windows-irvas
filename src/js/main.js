8eef09146db784321cb0bf0383d96d26dc370b90
Yaroslav <dzisyuk.yari@gmail.com>
2022-01-08 19:21:27 +0200

    Only digits can be inputed in inputs, simple form handler

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import "./slider.js";
 import modals from "./modules/modals.js";
 import tabs from "./modules/tabs.js";
+import forms from "./modules/forms.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    forms();
 });
